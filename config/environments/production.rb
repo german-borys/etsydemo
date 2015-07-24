a1ec96af85fa3ce3d6360ec49ce90ea2e4d80331
Borys German <german.borys@gmail.com>
2015-07-24 15:31:55 -0600

    Added Devise for User Mgmt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Etsydemo::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # required for Devise, remember to change localhost to actual application host
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
