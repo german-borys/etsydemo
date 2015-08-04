6fdd8597865b1d9e1466ad53f4d77500e5a33cb0
Borys German <german.borys@gmail.com>
2015-08-04 14:27:38 -0600

    add order history pages

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,45 @@
+class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  respond_to :html
+
+  def sales
+    @orders = Order.all.where(seller: current_user).order("created_at DESC")
+  end
+
+  def purchases
+    @orders = Order.all.where(buyer: current_user).order("created_at DESC")
+  end
+
+  
+
+  def new
+    @order = Order.new
+    @listing = Listing.find(params[:listing_id])
+    respond_with(@order)
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @listing = Listing.find(params[:listing_id])
+    @seller = @listing.user
+
+    @order.buyer_id = current_user.id
+    @order.seller_id = @seller.id
+    @order.save
+    #respond_with(@order)
+    respond_to do |format|
+      format.html {redirect_to root_url, notice: 'Order was successfully created.'}
+      format.json {render action: 'show', status: :created, location: @order}
+    end
+  end
+
+  private
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    def order_params
+      params.require(:order).permit(:address, :city, :state)
+    end
+end
